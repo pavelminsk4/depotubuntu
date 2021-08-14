2403188823c59e377d307fdeb4e541b995e3d0cc
pavelminsk4 <pavelminsk4@gmail.com>
2021-08-14 11:30:03 +0300

    feat: add Test fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,3 +51,20 @@ skills for communicating, information, and fun.
                  image_url: 'dpchat.jpg',
                  price: 31.00)
 # # . . .
+  Product.create!(title: '4 Build Chatbot Interactions',
+                  description:
+                    %{<p>
+  <em>Responsive, Intuitive Interfaces with Ruby</em>
+  The next step in the evolution of user interfaces is here.
+  hatbots let your users interact with your service in their
+  own natural language. Use free and open source tools along
+  with Ruby to build creative, useful, and unexpected interactions
+  for users. Take advantage of the Lita framework’s step-by-step
+  implementation strategy to simplify bot development and testing.
+  From novices to experts, chatbots are an area in which everyone
+  can participate. Exercise your creativity by creating chatbot
+  skills for communicating, information, and fun.
+  </p>},
+                  image_url: 'crystal.jpg',
+                  price: 45.00)
+ # # . . .
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,6 +2,8 @@ require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
 
+  fixtures :products
+
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -52,5 +54,16 @@ http://a.b.c/x/y/z/fred.gif }
              "#{image_url} shouldn't be valid"
     end
   end
+
+  test "product is not valid without a unique title - i18n" do
+   product = Product.new(title: products(:ruby).title,
+                         price: 1,
+                         image_url: "fred.gif")
+   assert product.invalid?
+   assert_equal [I18n.translate('errors.messages.taken')],
+                product.errors[:title]
+   end
 end
+
+
 #nill
